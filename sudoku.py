cb97b393bf40c0ac1a1402d123aed796389e1662
Eager Tom <thomas.eldridge@meteoblue.com>
2025-01-15 17:38:47 +0100

    Add Sudoku solver functionality with initial guesses. Works but doesn't backtrack properly!
    Implemented a backtracking algorithm to solve Sudoku puzzles. Refactored validation logic for square regions for improved clarity. Updated the main script to include an example Sudoku board and call the solve method.

--- sudoku.py
+++ sudoku.py
@@ -6,7 +6,7 @@ import numpy as np
 class Sudoku:
     def __init__(self, board: Optional[np.ndarray] = None):
         self.board = board if board is not None else np.full((9, 9), np.nan)
-        self.current_cell = (0, 0)
+        self.initial_guesses = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 
     @property
     def next_empty_cell(self) -> tuple[int, int]:
@@ -60,14 +60,40 @@ class Sudoku:
     def squares_valid(self) -> bool:
         for i in range(3):
             for j in range(3):
-                if len(set(self.board[3 * i:3 * i + 3, 3 * j:3 * j + 3][
-                               ~np.isnan(self.board[3 * i:3 * i + 3, 3 * j:3 * j + 3])].flatten())) != np.sum(
-                    ~np.isnan(self.board[3 * i:3 * i + 3, 3 * j:3 * j + 3])):
+                square_vals = self.board[3 * i:3 * i + 3, 3 * j:3 * j + 3].flatten()
+                square_vals = square_vals[~np.isnan(square_vals)]
+                num_cells_filled = np.sum(~np.isnan(self.board[3 * i:3 * i + 3, 3 * j:3 * j + 3]))
+                if len(set(square_vals)) != num_cells_filled:
                     return False
         return True
 
+    def solve(self) -> bool:
+        solved = False
+        cell = self.next_empty_cell
+        guesses = self.initial_guesses
+        if cell:
+            while len(guesses) > 0:
+                self.set_value(cell, guesses[0])
+                if self.check_valid():
+                    solved = self.solve()
+                if not solved:
+                    guesses = guesses[1:]
+                    self.delete_value(cell)
+        else:
+            return True
+
 
 if __name__ == "__main__":
-    sudoku = Sudoku()
+    sudoku = Sudoku(np.array([
+        [5, 3, np.nan, np.nan, 7, np.nan, np.nan, np.nan, np.nan],
+        [6, np.nan, np.nan, 1, 9, 5, np.nan, np.nan, np.nan],
+        [np.nan, 9, 8, np.nan, np.nan, np.nan, np.nan, 6, np.nan],
+        [8, np.nan, np.nan, np.nan, 6, np.nan, np.nan, np.nan, 3],
+        [4, np.nan, np.nan, 8, np.nan, 3, np.nan, np.nan, 1],
+        [7, np.nan, np.nan, np.nan, 2, np.nan, np.nan, np.nan, 6],
+        [np.nan, 6, np.nan, np.nan, np.nan, np.nan, 2, 8, np.nan],
+        [np.nan, np.nan, np.nan, 4, 1, 9, np.nan, np.nan, 5],
+        [np.nan, np.nan, np.nan, np.nan, 8, np.nan, np.nan, 7, 9]
+    ]))
     sudoku.solve()
     print(sudoku.board)
